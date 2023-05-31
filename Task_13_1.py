dc8e99cced6040d46217591e7eb69da2b9101a81
Wikatoria <wikatoria.pavlova@redcircle.ai>
2023-05-31 10:27:47 +0300

    Task_13_1

---
+++ Task_13_1.py
@@ -0,0 +1,17 @@
+def gen():
+    i = 0
+    j = 0
+    while True:
+        i += 1
+        for j in range(i):
+            if i%2==0:
+                j = i
+                j *= -1
+            else:
+                j = i
+                j *= 1
+        yield j
+
+gener = gen()
+for i in range(int(input())):
+    print(next(gener), end = ',')
