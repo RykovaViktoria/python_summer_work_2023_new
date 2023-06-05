2ca353e621b7e8c08dd7735ade71f4db2dc1a164
Wikatoria <wikatoria.pavlova@redcircle.ai>
2023-06-05 16:24:21 +0300

    Task_15_3

--- Task_15_3.py
+++ Task_15_3.py
@@ -1,4 +1,6 @@
-import re
+def mob(s):
+    import re
+    regex = r'^\+7[(]812[)]\d{3}-\d{4}\b|^\+7[(]921[)]\d{3}-\d{4}\b'
+    return re.findall(regex, s)
 s = input('Введите телефонные номера -->')
-regex = r'^\+7[(]812[)]\d{3}-\d{4}\b|^\+7[(]921[)]\d{3}-\d{4}\b'
-print(re.findall(regex, s))
+print(mob(s))
