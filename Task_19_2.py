5e5aa14d68968425f4e3e80681e45f6db7c517bc
Wikatoria <wikatoria.pavlova@redcircle.ai>
2023-06-16 16:21:56 +0300

    Task_19_2

---
+++ Task_19_2.py
@@ -0,0 +1,20 @@
+class Fib:
+    su = 0
+    def __init__(self, limit):
+        self.first = 1
+        self.second = 0
+        self.value = 0
+        self.limit = limit
+    def __iter__(self):
+        return self
+    def __next__(self):
+        self.value = self.first + self.second
+        self.first = self.second
+        self.second = self.value
+        if self.value > self.limit:
+            raise StopIteration
+        return self.value
+f = Fib(100)
+for i in f:
+    print(i)
+    input()
