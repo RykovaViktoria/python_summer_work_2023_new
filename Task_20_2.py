1a2e69ba7f8670b5c60b259807e1a9a96f5995c7
Wikatoria <wikatoria.pavlova@redcircle.ai>
2023-06-18 21:44:08 +0300

    Task_20_2

---
+++ Task_20_2.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+df = pd.DataFrame([[1, 'lala', 23],
+                  [2, 'rurk', 44],
+                  [3, 'rer', 2]])
+
+su = 0
+for i in df.index:
+    for j in df.columns:
+        if type(df.loc[i,j]) == np.int64:
+            su += df.loc[i,j]
+print(su)
+
+
