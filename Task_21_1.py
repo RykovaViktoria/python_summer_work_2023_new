d52b6da354faa77e99e686dec7a454957d38cef0
Wikatoria <wikatoria.pavlova@redcircle.ai>
2023-06-20 22:45:35 +0300

    Task_21_1

---
+++ Task_21_1.py
@@ -0,0 +1,16 @@
+create table Pupil (
+    id int,
+    name text,
+    avg_mark float4,
+    visits int,
+    number_contract int,
+    group_name text,
+    payment bool,
+    country text,
+    city text,
+    email text,
+    telephone_number int,
+    theme_of_diploma text,
+    level_of_education text,
+    form_of_teaching text
+   )
