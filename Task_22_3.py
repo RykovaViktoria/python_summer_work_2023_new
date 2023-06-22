08647372635a6cca105961d4c9cc50c4467cd2c4
Wikatoria <wikatoria.pavlova@redcircle.ai>
2023-06-22 11:47:58 +0300

    Task_22_3

---
+++ Task_22_3.py
@@ -0,0 +1,8 @@
+import keyword
+kw = [i.lower() for i in keyword.kwlist]
+s = list(('For Our route with math').split())
+for n, i in enumerate(s):
+    if i.lower() in kw:
+        s[n] = '<kw>'
+print(' '.join(s))
+
